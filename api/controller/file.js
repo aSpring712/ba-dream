34443710aa4b393e056afafc1986da9067acbfd2
aSpring <rnjsqhal2001@naver.com>
2023-11-19 20:30:14 +0900

    주석 추가

--- api/controller/file.js
+++ api/controller/file.js
@@ -1,22 +1,27 @@
 const axios = require('axios');
 
 module.exports = {
+    // 컨텐츠 업로드
     async uploadFiles(req, res) {
-        console.log('파일 업로드 req.query', req.query);
-        console.log('파일 업로드 req.query', req.files);
+        console.log('파일 업로드')
 
         return res.json( { type: 'SUCCESS'});
     },
+    // 휴지통에 추가 (임시 삭제 - 상태값 변경 또는 플레이 리스트 table에 추가)
     async moveToTrashcan(req, res) {
         console.log(req.body);
+        // 휴지통으로 이동
+        // 가장 최근 재생 된 컨텐츠 목록, 플레이 리스트에서 제거
 
         return res.json({type: "SUCCESS"});
     },
+    // 플레이 리스트에 추가 (상태값 변경 또는 플레이 리스트 table에 추가)
     async addPlayList(req, res) {
         console.log('ADD PLAY LIST ===>',);    
 
         return res.json({type: "SUCCESS"});
     },
+    // 전체 컨텐츠 조회
     async allFiles(req, res) {
         console.log('전체 파일 조회');
 
@@ -35,6 +40,7 @@ module.exports = {
 
         return res.json({type: "SUCCESS", data})
     },
+    // 휴지통 목록 조회
     async allTrash(req, res) {
         console.log('휴지통 목록');
 
@@ -46,6 +52,7 @@ module.exports = {
 
         return res.json({type: "SUCCESS", data})
     },
+    // 최근 재생된 컨텐츠 조회
     recentView(req, res) {
         console.log('최근 재생된 컨텐츠 불러오기');
 
@@ -67,6 +74,7 @@ module.exports = {
 
         return res.json({type: "SUCCESS", data})
     },
+    // 플레이 리스트 조회
     getPlayList(req, res) {
         let data =  [
         {
@@ -82,6 +90,7 @@ module.exports = {
 
         return res.json({type: "SUCCESS", data})
     },
+    // 영구 삭제
     async delFile(req, res) {
         console.log(req.params.id);
 
